91a72ed0ede93db97a9a5e9ca70d95fc0acc3c4c
Sheedh Mashood <148475133+SheeshMuchacho@users.noreply.github.com>
2025-09-19 09:34:03 +0530

    ui: loading animation
    for blogindex page between pagination

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,19 @@
+import { Suspense } from "react";
+import Loading from "./loading";
 import BlogIndex from "@/components/blog/blog-section";
 
 export default async function Blog({
   searchParams,
 }: {
+  // if you’re on Next 15’s async dynamic APIs:
   searchParams?: Promise<{ page?: string }>;
 }) {
   const { page } = (await searchParams) ?? {};
   const pageNum = Number(page ?? "1");
-  return <BlogIndex page={pageNum} />;
+
+  return (
+    <Suspense key={pageNum} fallback={<Loading />}>
+      <BlogIndex page={pageNum} />
+    </Suspense>
+  );
 }
